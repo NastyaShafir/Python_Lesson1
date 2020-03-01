b69d5911ef753daa86fa03d257db567efa9166e5
Nastya Shafir <zaichic1@yandex.ru>
2020-03-01 16:24:44 +1000

    Выполнила задание 1 урока 1

---
+++ Task1.py
@@ -0,0 +1,12 @@
+# Task 1: Поработайте с переменными, создайте несколько,
+# выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+number = 3
+print(number)
+a = "Hello"
+print(a)
+b = [1, 2, 3]
+print(b)
+number_1 = input("Enter your name - ")
+number_2 = int(input("How old are you - "))
+print(number_1, number_2)
+
